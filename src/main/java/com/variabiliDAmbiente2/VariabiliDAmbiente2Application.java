a30f0917086a3ca7bfe8cb9c5e08fdb207e7d045
Daniele de Oliveira <deo9674@gmail.com>
2024-09-05 14:56:11 +0200

    init commit

---
+++ src/main/java/com/variabiliDAmbiente2/VariabiliDAmbiente2Application.java
@@ -0,0 +1,13 @@
+package com.variabiliDAmbiente2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VariabiliDAmbiente2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VariabiliDAmbiente2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/variabiliDAmbiente2/VariabiliDAmbiente2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.variabiliDAmbiente2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VariabiliDAmbiente2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
